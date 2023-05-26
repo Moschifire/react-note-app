974b0b216473160554eba367006829ef05659cf5
SchinoTheRapper <110900056+Moschifire@users.noreply.github.com>
2023-05-26 21:01:13 +0100

    components added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import NotesList from './components/NotesList';
 
-function App() {
+const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is my first note!",
+      date: "26/05/2023"
+    },
+    {
+      id: nanoid(),
+      text: "This is my second note!",
+      date: "27/05/2023"
+    },
+    {
+      id: nanoid(),
+      text: "This is my third note!",
+      date: "28/05/2023"
+    },
+    {
+      id: nanoid(),
+      text: "This is my new note!",
+      date: "28/05/2023"
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <NotesList notes={notes} />
     </div>
   );
 }
